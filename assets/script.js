9ff345927a4d155aca687f8bae7143596e032b73
Natia Nakashidze <n.nakashidze09@gmail.com>
2020-12-16 15:35:00 +0400

    scrollis dros pages nomrebze activ klasis minicheba

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,16 @@ const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const page3 = document.getElementById('page3');
 const page4 = document.getElementById('page4');
+const page5 = document.getElementById('page5');
+const page6 = document.getElementById('page6');
 
 page1.addEventListener('click', scrollPage);
 page2.addEventListener('click', scrollPage);
 page3.addEventListener('click', scrollPage);
 page3.addEventListener('click', scrollPage);
 page4.addEventListener('click', scrollPage);
+page5.addEventListener('click', scrollPage);
+page6.addEventListener('click', scrollPage);
 
 function scrollPage(e){
     e.preventDefault();
@@ -24,4 +28,44 @@ function classRemove(){
     page2.classList.remove('active');
     page3.classList.remove('active');
     page4.classList.remove('active');
+    page5.classList.remove('active');
+    page6.classList.remove('active');
 }
+
+window.addEventListener('scroll', scrollHandler);
+
+function scrollHandler(){
+    let pageTop2 = document.querySelector('.page2').offsetTop;
+    let pageTop3 = document.querySelector('.page3').offsetTop;
+    let pageTop4 = document.querySelector('.page4').offsetTop;
+    let pageTop5 = document.querySelector('.page5').offsetTop;
+    let pageTop6 = document.querySelector('.page6').offsetTop;
+    classRemove();
+    if(window.scrollY < pageTop2){
+        page1.classList.add("active");
+    } else {
+        if(window.scrollY < pageTop3){
+            page2.classList.add("active");
+        } else {
+            if(window.scrollY < pageTop4){
+                page3.classList.add("active");
+            } else {
+                if(window.scrollY < pageTop5){
+                    page4.classList.add("active");
+                }  else {
+                    if(window.scrollY < pageTop6){
+                        if(document.body.offsetHeight - window.innerHeight - window.scrollY  < 50) {
+                            page6.classList.add("active");
+                        } else {
+                            page5.classList.add("active");
+                        }
+                    } else {
+                        page6.classList.add("active");
+                    }
+                }
+            } 
+        } 
+    } 
+}
+
+scrollHandler();
